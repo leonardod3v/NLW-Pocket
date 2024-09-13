ad0124e3cf20286f002b2e73ef0da0a44f3bba12
Bowie <marketing@letscode.com.br>
2024-09-13 20:43:22 -0300

    adiciona inquire

---
+++ aula.md
@@ -0,0 +1,4 @@
+## Fases da resolução de um problema
+- Coletar os dados
+- Processar os dados (manipular, alterar...)
+- Apresentar os dados
---
+++ index.js
@@ -0,0 +1,18 @@
+const start = () =>{
+
+    while(true){
+        let opcao = "Sair"
+        switch(opcao) {
+            case "Cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "Listar":
+                console.log("Vamos listar")
+                break
+            case "Sair":
+                return
+        }
+    }
+}
+
+start()
